8061713a0d521c4ae35fafefa5ef56e55080ddb6
André de Castro <andre_vc@hotmail.com>
2022-09-13 23:33:31 -0300

    style the Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,21 @@
+import { NavLink } from 'react-router-dom'
+import { Timer, Scroll } from 'phosphor-react'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { HeaderContainer } from './styles'
+
 export function Header() {
-  return <h1>Header</h1>
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to="" title="Timer">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
 }
