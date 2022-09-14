624328413941bf5da3d767bfe140db9c7a3b84d3
André de Castro <andre_vc@hotmail.com>
2022-09-13 23:00:50 -0300

    create default layout structure

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { History } from '../pages/History'
 import { NotFound } from '../pages/NotFound'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 
 export function Router() {
   return (
     <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
       <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
     </Routes>
   )
 }
